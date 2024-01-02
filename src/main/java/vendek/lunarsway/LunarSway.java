bd94f4b35fcc79076db54507d05560c1ba260bdc
bitweed <Oltibanta342@proton.me>
2024-01-03 01:46:06 +0300

    Выловка булыжника в пустоте.

--- src/main/java/vendek/lunarsway/LunarSway.java
+++ src/main/java/vendek/lunarsway/LunarSway.java
@@ -2,13 +2,23 @@ package vendek.lunarsway;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.scheduler.BukkitRunnable;
 import vendek.lunarsway.commands.WorldChanger;
+import vendek.lunarsway.events.VoidFishing;
 
 public final class LunarSway extends JavaPlugin {
+    private static LunarSway instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+
         getCommand("ls-fly_to").setExecutor(new WorldChanger());
+        getServer().getPluginManager().registerEvents(new VoidFishing(), this);
+    }
+
+    public static LunarSway getInstance() {
+        return instance;
     }
 
     @Override
