50a9fa773d75f45547f3cb74b36b8fe9ba3c983a
bitweed <Oltibanta342@proton.me>
2024-01-14 22:04:49 +0300

    Добавлен крафт маленькой теплицы

--- src/main/java/vendek/lunarsway/craft/MyCrafts.java
+++ src/main/java/vendek/lunarsway/craft/MyCrafts.java
@@ -7,20 +7,34 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import vendek.lunarsway.LunarSway;
+
+import java.util.Arrays;
 
 public class MyCrafts {
     public static void smallGreenHouse() {
-        ItemStack smallGreenhouse = new ItemStack(Material.GLASS);
+        ItemStack smallGreenhouse = new ItemStack(Material.PAPER);
         ItemMeta meta = smallGreenhouse.getItemMeta();
         meta.setDisplayName(ChatColor.RESET + "Маленькая теплица");
-        meta.setCustomModelData(1);
+
+        // Описание предмета
+        String[] lore = {
+                ChatColor.GRAY + "Позволяет выращивать растения в условиях невесомости",
+                ChatColor.WHITE + "[" + ChatColor.GREEN + "ПКМ чтобы установить" + ChatColor.WHITE + "]"
+        };
+        meta.setLore(Arrays.asList(lore));
+
+        // NBT тег CustomModelData
+        meta.getPersistentDataContainer().set(new NamespacedKey(LunarSway.getInstance(), "custommodeldata"), PersistentDataType.INTEGER, 24001);
+
         smallGreenhouse.setItemMeta(meta);
 
         ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(LunarSway.getInstance(), "small_greenhouse"), smallGreenhouse);
-        recipe.shape("G", "D");
-        recipe.setIngredient('G', Material.GLASS);
-        recipe.setIngredient('D', Material.DIRT);
+        recipe.shape("CCC", "CSC", "JJJ");
+        recipe.setIngredient('C', Material.GLASS);
+        recipe.setIngredient('S', Material.DIRT);
+        recipe.setIngredient('J', Material.IRON_INGOT);
 
         Bukkit.getServer().addRecipe(recipe);
     }
